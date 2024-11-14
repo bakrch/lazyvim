dabfb7041952da489211d71b916abd0d118d8fba
unknown <bakr.chakk@gmail.com>
2024-11-14 20:15:47 +0100

    Added nu as default shell for windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- j
+local os = os.getenv("OS")
+if os and os:match("Windows") then
+  vim.api.nvim_set_var("terminal_emulator", "powershell")
+  vim.opt.shell = "nu.exe"
+  print("Is windows")
+end
